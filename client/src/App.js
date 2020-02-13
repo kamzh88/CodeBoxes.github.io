deab3eecaa0e3866778f455c2440637c66c3818d
KinKam Zhao <kamzh88@hotmail.com>
2020-02-13 14:29:24 -0500

    basic nav bar setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Wrapper from "./components/Wrapper";
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <p>testing</p>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact render={() => (
+          <Wrapper>
+            <Link to="/"style={{ marginRight: 16 }}>Search</Link>
+            <Link to="/save"style={{ marginRight: 16 }}>Saved</Link>
+          </Wrapper>
+        )}
+        ></Route>/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
