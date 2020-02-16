8c43e1c03012c13112cdeb2a42a0f7bf42ffebda
KinKam Zhao <kamzh88@hotmail.com>
2020-02-16 00:19:13 -0500

    add save button to top right corner of page

--- client/src/components/SearchResults/index.js
+++ client/src/components/SearchResults/index.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from "react";
-import { Card } from '@material-ui/core';
-import { BookResult } from "../BookResult";
+import { Card, Button, Grid } from '@material-ui/core';
+import SaveIcon from '@material-ui/icons/Save';
 
 const styles = {
     Card: {
@@ -11,17 +11,28 @@ const styles = {
 }
 
 class SearchResults extends Component {
-
     render() {
-
         return (
             <div style={styles.Card}>
                 <Card variant="outlined">
                     <h2 style={{ marginLeft: 60 }}>Results</h2>
                     <ul>
                         {this.props.results.map(result => (
-                            <Card variant="outlined" style={{ width: "auto", margin: 20}}>
-                                <li key={result.volumeInfo.industryIdentifiers[0].identifier} style={{overflow: "auto", padding: 10}}>
+                            <Card variant="outlined" style={{ width: "auto", margin: 20 }}>
+                                <li key={result.volumeInfo.industryIdentifiers[0].identifier} style={{ overflow: "auto", padding: 10 }}>
+                                    <Grid
+                                    container
+                                    justify="flex-end">
+                                        <Button
+                                            variant="contained"
+                                            color="primary"
+                                            size="small"
+                                            startIcon={<SaveIcon />}
+                                            style={{ position: 'static', top: 8, left: 1000 }}
+                                        >
+                                            Save
+                                        </Button>
+                                    </Grid>
                                     Title: {result.volumeInfo.title}<br></br>
                                     Subtitle: {result.volumeInfo.subtitle}<br></br>
                                     Authors: {result.volumeInfo.authors}<br></br>
@@ -31,10 +42,8 @@ class SearchResults extends Component {
                             </Card>
                         ))}
                     </ul>
-
                 </Card>
             </div >
-
         );
     };
 };
