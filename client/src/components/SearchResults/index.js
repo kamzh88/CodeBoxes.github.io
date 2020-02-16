fd83e09ba6d87b5da0f4770c39957ebf0029b817
KinKam Zhao <kamzh88@hotmail.com>
2020-02-15 23:43:40 -0500

    styling for Book Results

--- client/src/components/SearchResults/index.js
+++ client/src/components/SearchResults/index.js
@@ -16,22 +16,25 @@ class SearchResults extends Component {
         // const results = this.props.results
         // console.log(results)
         return (
-            
-                <Card style={styles.Card}>
-                    <h2 style={{ marginLeft: 10 }}>Results</h2>
-                    <ul> 
+            <div style={styles.Card}>
+                <Card variant="outlined">
+                    <h2 style={{ marginLeft: 60 }}>Results</h2>
+                    <ul>
                         {this.props.results.map(result => (
-                        <li key={result.volumeInfo.industryIdentifiers[0].identifier}>
-                            Title: {result.volumeInfo.title}<br></br>
-                            Subtitle: {result.volumeInfo.subtitle}<br></br>
-                            Authors: {result.volumeInfo.authors}<br></br>
-                            Description: {result.volumeInfo.description}
-                            <br></br><br></br>
-                        </li>
+                            <Card variant="outlined" style={{ width: "auto", margin: 20}}>
+                                <li key={result.volumeInfo.industryIdentifiers[0].identifier} style={{overflow: "auto", padding: 10}}>
+                                    Title: {result.volumeInfo.title}<br></br>
+                                    Subtitle: {result.volumeInfo.subtitle}<br></br>
+                                    Authors: {result.volumeInfo.authors}<br></br>
+                                    Description: {result.volumeInfo.description}
+                                    <br></br><br></br>
+                                </li>
+                            </Card>
                         ))}
-                    </ul>                 
+                    </ul>
+
                 </Card>
-         
+            </div >
 
         );
     };
