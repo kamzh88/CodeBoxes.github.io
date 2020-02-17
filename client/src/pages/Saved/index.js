97f3ba7d651deacf51de8583c48e44542c64fe60
KinKam Zhao <kamzh88@hotmail.com>
2020-02-17 14:08:45 -0500

    append information onto page

--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -8,58 +8,43 @@ class Saved extends Component {
     state = {
         books: [],
         title: [],
-        author: []
+        author: [],
+
     };
 
     componentDidMount() {
         API.getBooks()
             .then(res => {
-                // console.log(res.data);
+
                 let title = this.state.title;
                 let author = this.state.author;
+
                 res.data.map(book => {
-                    // console.log(book.title);
                     title.push(book.title)
                     author.push(book.authors);
+
                 })
                 this.setState({ books: res.data, title: title, author: author })
-                // console.log(author);
             })
             .catch(err => console.log(err));
     }
 
     render() {
-        // console.log(this.state.title)
-        // console.log(this.state.author)
+        console.log(this.state.books);
         return (
             <div>
                 <Heading />
-                <Wrapper>
-
-                    {/* <SaveResults> */}
-                    {this.state.books.map(book => (
-                        <div>
-                            <p>Title: {book.title}</p>
-                            <p>Authors: {book.authors}</p><br></br>
-                        </div>
-
-                    ))}
-
-
-
-
-
-                    {/* title={this.state.title}
-                        author={this.state.author}
-                     {this.state.title} */}
-                    {/* </SaveResults>  */}
-                    {/* {
-                    this.state.title.map((title, index) => (<h2 key={index}>{title}</h2>)),
-                    this.state.author.map((author, index) => (<h4 key={index}>{author}</h4>))
-                    } */}
-
-
-                </Wrapper >
+                {this.state.books.map(book => (
+                    <div key={book._id}>
+                        <p>Title: {book.title}</p>
+                        <p>Image: {book.image}</p>
+                        <p>Subtitle: {book.subtitle}</p>
+                        <p>Authors: {book.authors}</p>
+                        <p>Description: {book.description}</p>
+                        <p>Link: {book.link}</p>
+                        <br></br>
+                    </div>
+                ))}
             </div>
         )
     }
