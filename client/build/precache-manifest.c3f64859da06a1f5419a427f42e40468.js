bd782a5e395e3ce58ca07c52f624db752f91b2ca
KinKam Zhao <kamzh88@hotmail.com>
2020-02-20 18:38:41 -0500

    updates

---
+++ client/build/precache-manifest.c3f64859da06a1f5419a427f42e40468.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52093dbedf6f6338f7c4d212e4eb9b96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b0be74193bcc6ee011d",
+    "url": "/static/css/main.d45fafe0.chunk.css"
+  },
+  {
+    "revision": "0aa6d57e73fa0cc5016d",
+    "url": "/static/js/2.d8880941.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/static/js/2.d8880941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0be74193bcc6ee011d",
+    "url": "/static/js/main.21dd99ed.chunk.js"
+  },
+  {
+    "revision": "fb96425554e7260b173a",
+    "url": "/static/js/runtime-main.6c4b6dc1.js"
+  }
+]);
